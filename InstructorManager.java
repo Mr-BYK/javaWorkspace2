f1966ce57fd951d7ede6123f69e4036bdd059f36
Burak Yusuf KARABIYIK <20174753013@ogr.akdeniz.edu.tr>
2021-05-09 07:08:41 +0300

    Update InstructorManager.java

--- InstructorManager.java
+++ InstructorManager.java
@@ -6,16 +6,16 @@ public class InstructorManager extends UserManager{
 	
 	@Override
 	public void add(User user) {
-		System.out.println("Instructor " + user.getFirstName() +" " +  " added.");
+		System.out.println("Instructor " + user.getFirstName() +" added.");
 	}
 	
 	@Override
 	public void remove(User user) {
-		System.out.println("Instructor " + user.getFirstName() + " " + " remove.");
+		System.out.println("Instructor " + user.getFirstName() +" remove.");
 	}
 	
 	@Override
 	public void update(User user) {
-		System.out.println("Instructor " + user.getFirstName() + " " +  " update.");
+		System.out.println("Instructor " + user.getFirstName() +" update.");
 }
 }
